69066d9710f6afd5466cf4d3f6f3de6e71abeada
aurore-ry <aremy.dev@gmail.com>
2023-10-18 13:04:49 +0200

    Create user feature is work

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { CreateUserDto } from 'src/users/users.user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  async addUser(@Req() req, @Body() CreateUserDto: CreateUserDto) {
+    const newUser = await this.userService.create(CreateUserDto);
+    console.log(newUser);
+  }
+}
--- src/users/users.user.dto.ts
+++ src/users/users.user.dto.ts
@@ -1,4 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsNotEmpty,
+  IsStrongPassword,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
@@ -11,17 +16,22 @@ export class LoginUserDto {
   readonly password: string;
 }
 export class CreateUserDto {
+  @IsAlphanumeric()
+  @IsEmail()
   @IsNotEmpty()
   @ApiProperty()
   email: string;
+  @IsAlphanumeric()
   @IsNotEmpty()
   @ApiProperty()
   username: string;
+  @IsStrongPassword()
   @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
 export class UpdatePasswordDto {
+  @IsStrongPassword()
   @IsNotEmpty()
   @ApiProperty()
   new_password: string;
